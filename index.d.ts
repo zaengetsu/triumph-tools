10b03910a2ce62103e892457d4c6c150cf29c00e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-09-11 10:34:03 +0200

    mise en place d'un fichier .d.ts qui va me permettre d'exporter les types de mes modules

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// mst-triumph-tools/index.d.ts
+
+declare module 'mst-triumph-tools' {
+    export const db: any; 
+    export const appConfig: any;
+    export const updateContext: any;
+    // Ajoute toutes les autres fonctions/constantes exportées ici
+  }
+  
